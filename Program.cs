fc12d4e7b9a633215fb33cc053e650a20e758178
Nikita <nikita.serg.utkin@gmail.com>
2022-12-02 23:58:14 +0900

    Работает

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("не четное");
+}
